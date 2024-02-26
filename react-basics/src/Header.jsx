60b6d8a4f9cb90d9c8a183051145bf8d5ca018f6
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-26 15:24:21 +0200

    Modified Header and Updated App files

---
+++ react-basics/src/Header.jsx
@@ -0,0 +1,18 @@
+
+function Header() {
+ 
+    return (
+        <header>
+        <h1>REACT Example</h1>
+        <ul>
+            <li><a href="href">Very Easy</a></li>
+            <li><a href="href">Easy</a></li>
+            <li><a href="href">Middle</a></li>
+            <li><a href="href">Hard</a></li>
+            <li><a href="href">Very Hard</a></li>
+        </ul>
+        </header>
+    )
+}
+
+export default Header
