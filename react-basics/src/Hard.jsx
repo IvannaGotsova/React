4a17b100b2b89e4a455a7419f33c98c570f2e191
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-27 18:40:40 +0200

    Modified with content

---
+++ react-basics/src/Hard.jsx
@@ -0,0 +1,40 @@
+
+function Hard() {
+ 
+    return (
+        <body>
+            <h2>HARD</h2>
+            <hr />
+            <ol>
+                <li> 
+                    <h3>First Example</h3>               
+                    <h5>This is first example</h5>
+                    <p>Summary of first example...................................................................................................</p>
+                </li>
+                <li>
+                    <h3>Second Example</h3>
+                    <h5>This is second example</h5>
+                    <p>Summary of second example...................................................................................................</p>
+                </li>
+                <li>
+                    <h3>Third Example</h3>
+                    <h5>This is third example</h5>
+                    <p>Summary of third example...................................................................................................</p>
+                </li>
+                <li>
+                    <h3>Fourth Example</h3>
+                    <h5>This is fourth example</h5>
+                    <p>Summary of fourth example...................................................................................................</p>
+                </li>
+                <li>
+                    <h3>Fifth Example</h3>
+                    <h5>This is fifth example</h5>
+                    <p>Summary of fifth example...................................................................................................</p>
+                </li>
+            </ol>
+            <hr />
+        </body>
+    )
+}
+
+export default Hard
