fd7a90992f833a1966aae0236b52cefe0124c21e
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-29 22:27:55 +0200

    Modified App file with added Header and Footer

--- online-libraries/src/App.jsx
+++ online-libraries/src/App.jsx
@@ -1,13 +1,16 @@
 
 import './App.css'
+import Header from './Header'
+import Footer from './Footer'
 
 function App() {
 
   return (
     <>
-     
+     <Header />     
+     <Footer />   
     </>
   )
 }
 
-export default App
+export default App;
