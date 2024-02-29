81fc1d9db4473a90468161ed889c30ab1d8ce8a0
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-29 23:01:22 +0200

    Updated file with <b> tags

--- online-libraries/src/Body.jsx
+++ online-libraries/src/Body.jsx
@@ -6,9 +6,13 @@ function Body() {
     <>
      <ul>
         <li><a href="">Books</a></li>
+        <b>Your books are here</b>
         <li><a href="">Games</a></li>
+        <b>Your games are here</b>
         <li><a href="">Movies</a></li>
+        <b>Your movies are here</b>
         <li><a href="">Songs</a></li>
+        <b>Your songs are here</b>
      </ul>   
     </>
   )
