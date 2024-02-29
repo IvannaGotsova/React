30134b1fdad81521799edafbb294f5816fa63867
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-29 22:24:54 +0200

    Created and Modified Header file

---
+++ online-libraries/src/Header.jsx
@@ -0,0 +1,13 @@
+
+function Header () {
+
+    return (
+        <>
+        <h1>Online Libraries</h1>
+        <h4>Here you can store all your favourite things</h4>
+        <hr />
+        </>
+    )
+}
+
+export default Header
