dbfe533119e704fc6940701b0a03f79e8a375112
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-29 22:37:56 +0200

    Modified Body file with links

---
+++ online-libraries/src/Body.jsx
@@ -0,0 +1,17 @@
+
+
+function Body() {
+
+  return (
+    <>
+     <ul>
+        <li><a href="">Books</a></li>
+        <li><a href="">Games</a></li>
+        <li><a href="">Movies</a></li>
+        <li><a href="">Songs</a></li>
+     </ul>   
+    </>
+  )
+}
+
+export default Body;
