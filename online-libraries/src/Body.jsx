1cb2167278e5d60574106fb6d6db7175500eb5d5
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-01 22:44:13 +0200

    Updated Body file with links to different sections, added imports and react-router-dom

--- online-libraries/src/Body.jsx
+++ online-libraries/src/Body.jsx
@@ -1,17 +1,29 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Books from "./Books";
+import Games from "./Games";
+import Movies from "./Movies";
+import Songs from "./Songs";
 
 function Body() {
 
   return (
     <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/books" element={<Books />} /> 
+            <Route path="/games" element={<Games />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/songs" element={<Songs />} />
+          </Routes>
+        </BrowserRouter>
      <ul>
-        <li><a href="">Books</a></li>
+        <li><a href="Books">Books</a></li>
         <b>Your books are here</b>
-        <li><a href="">Games</a></li>
+        <li><a href="Games">Games</a></li>
         <b>Your games are here</b>
-        <li><a href="">Movies</a></li>
+        <li><a href="Movies">Movies</a></li>
         <b>Your movies are here</b>
-        <li><a href="">Songs</a></li>
+        <li><a href="Songs">Songs</a></li>
         <b>Your songs are here</b>
      </ul>   
     </>
