3e6db1e62f620fd1349fa6de7d2e1d5c808c3112
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-02 23:12:31 +0200

    Modified Games file with added Cards components

--- online-libraries/src/Games.jsx
+++ online-libraries/src/Games.jsx
@@ -1,4 +1,4 @@
-
+import CardGames from "./CardGames";
 
 function Games() {
 
@@ -6,6 +6,35 @@ function Games() {
       <>
        <h3>All your Games</h3>
        <b>Your games are here</b>
+
+       <CardGames 
+       title = "Game Number One"
+       img = "./Picture.jpg"
+       author = "Game One Author"
+       description = "Description about Game Number One ..."
+       />
+
+       <CardGames 
+       title = "Game Number Two"
+       img = "./Picture.jpg"
+       author = "Game Two Author"
+       description = "Description about Game Number Two ..."
+       />
+
+       <CardGames 
+       title = "Game Number Three"
+       img = "./Picture.jpg"
+       author = "Game Three Author"
+       description = "Description about Game Number Three ..."
+       />
+
+       <CardGames 
+       title = "Game Number Four"
+       img = "./Picture.jpg"
+       author = "Game Four Author"
+       description = "Description about Game Number Four ..."
+       />
+
       </>
     )
   }
