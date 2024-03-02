3beed550ce8a6b9e037802604b5ab468b0eebf18
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-02 23:12:53 +0200

    Modified Songs file with added Cards components

--- online-libraries/src/Songs.jsx
+++ online-libraries/src/Songs.jsx
@@ -1,4 +1,4 @@
-
+import CardSongs from "./CardSongs";
 
 function Songs() {
 
@@ -6,6 +6,35 @@ function Songs() {
       <>
        <h3>All your Songs</h3>
        <b>Your songs are here</b>
+
+       <CardSongs 
+       title = "Song Number One"
+       img = "./Picture.jpg"
+       author = "Song One Author"
+       description = "Description about Song Number One ..."
+       />
+
+       <CardSongs 
+       title = "Song Number Two"
+       img = "./Picture.jpg"
+       author = "Song Two Author"
+       description = "Description about Song Number Two ..."
+       />
+
+       <CardSongs 
+       title = "Song Number Three"
+       img = "./Picture.jpg"
+       author = "Song Three Author"
+       description = "Description about Song Number Three ..."
+       />
+
+       <CardSongs 
+       title = "Song Number Four"
+       img = "./Picture.jpg"
+       author = "Song Four Author"
+       description = "Description about Song Number Four ..."
+       />
+
       </>
     )
   }
