b7d421240054462c6f4424b0ad105ddf7342690d
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-03 21:15:14 +0200

    Modified CardSongs with className Song-singer and className Song-text

--- online-libraries/src/CardSongs.jsx
+++ online-libraries/src/CardSongs.jsx
@@ -5,8 +5,8 @@ function CardSongs(props) {
             <div className="Song">
                 <h2 className ="Song-title">{props.title}</h2>
                 <img className ="Song-image" src={props.img} alt="" />
-                <h3 className ="Song-author">{props.author}</h3>
-                <p className ="Song-description">{props.description}</p>
+                <h3 className ="Song-singer">{props.author}</h3>
+                <p className ="Song-text">{props.description}</p>
                 <button className ="Song-button">View more...</button>
             </div>
         </>
