c645a63a5efc1547a43153b2c2a4013e64c99efd
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-03 20:52:41 +0200

    Added div tag with className

--- online-libraries/src/CardGames.jsx
+++ online-libraries/src/CardGames.jsx
@@ -2,11 +2,13 @@
 function CardGames(props) {
     return (
         <>
-            <h2 className ="Game-title">{props.title}</h2>
-            <img className ="Game-image" src={props.img} alt="" />
-            <h3 className ="Game-author">{props.author}</h3>
-            <p className ="Game-description">{props.description}</p>
-            <button className ="Game-button">View more...</button>
+            <div className="Game">
+                <h2 className ="Game-title">{props.title}</h2>
+                <img className ="Game-image" src={props.img} alt="" />
+                <h3 className ="Game-author">{props.author}</h3>
+                <p className ="Game-description">{props.description}</p>
+                <button className ="Game-button">View more...</button>
+            </div>
         </>
     )
 }
