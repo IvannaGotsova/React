ff5887b80d619f378a80cd65ab971bbf1c9a7d33
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-03 21:14:35 +0200

    Modified CardMovies with className Movie-actors

--- online-libraries/src/CardMovies.jsx
+++ online-libraries/src/CardMovies.jsx
@@ -5,7 +5,7 @@ function CardMovies(props) {
             <div className="Movie">
                 <h2 className ="Movie-title">{props.title}</h2>
                 <img className ="Movie-image" src={props.img} alt="" />
-                <h3 className ="Movie-author">{props.author}</h3>
+                <h3 className ="Movie-actors">{props.author}</h3>
                 <p className ="Movie-description">{props.description}</p>
                 <button className ="Movie-button">View more...</button>
             </div>
