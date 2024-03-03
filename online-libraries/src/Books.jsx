a836666d280fdefe51fd276993a9597043877b50
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-03 21:40:23 +0200

    Added new Books

--- online-libraries/src/Books.jsx
+++ online-libraries/src/Books.jsx
@@ -35,6 +35,90 @@ function Books() {
        description = "Description about Book Number Four ..."
        />
 
+       <CardBooks 
+       title = "Book Number One"
+       img = "./Picture.jpg"
+       author = "Book One Author"
+       description = "Description about Book Number One ..."
+       />
+
+       <CardBooks 
+       title = "Book Number Two"
+       img = "./Picture.jpg"
+       author = "Book Two Author"
+       description = "Description about Book Number Two ..."
+       />
+
+       <CardBooks 
+       title = "Book Number Three"
+       img = "./Picture.jpg"
+       author = "Book Three Author"
+       description = "Description about Book Number Three ..."
+       />
+
+       <CardBooks 
+       title = "Book Number Four"
+       img = "./Picture.jpg"
+       author = "Book Four Author"
+       description = "Description about Book Number Four ..."
+       />
+
+       <CardBooks 
+       title = "Book Number One"
+       img = "./Picture.jpg"
+       author = "Book One Author"
+       description = "Description about Book Number One ..."
+       />
+
+       <CardBooks 
+       title = "Book Number Two"
+       img = "./Picture.jpg"
+       author = "Book Two Author"
+       description = "Description about Book Number Two ..."
+       />
+
+       <CardBooks 
+       title = "Book Number Three"
+       img = "./Picture.jpg"
+       author = "Book Three Author"
+       description = "Description about Book Number Three ..."
+       />
+
+       <CardBooks 
+       title = "Book Number Four"
+       img = "./Picture.jpg"
+       author = "Book Four Author"
+       description = "Description about Book Number Four ..."
+       />
+
+<CardBooks 
+       title = "Book Number One"
+       img = "./Picture.jpg"
+       author = "Book One Author"
+       description = "Description about Book Number One ..."
+       />
+
+       <CardBooks 
+       title = "Book Number Two"
+       img = "./Picture.jpg"
+       author = "Book Two Author"
+       description = "Description about Book Number Two ..."
+       />
+
+       <CardBooks 
+       title = "Book Number Three"
+       img = "./Picture.jpg"
+       author = "Book Three Author"
+       description = "Description about Book Number Three ..."
+       />
+
+       <CardBooks 
+       title = "Book Number Four"
+       img = "./Picture.jpg"
+       author = "Book Four Author"
+       description = "Description about Book Number Four ..."
+       />
+       
       </>
     )
   }
