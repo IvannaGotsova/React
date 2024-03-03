4553d48e521b94bc4d9d658d8600cece61e5631f
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-03 21:40:52 +0200

    Added new Songs

--- online-libraries/src/Songs.jsx
+++ online-libraries/src/Songs.jsx
@@ -35,6 +35,88 @@ function Songs() {
        description = "Description about Song Number Four ..."
        />
 
+       <CardSongs 
+       title = "Song Number One"
+       img = "./Picture.jpg"
+       author = "Song One Author"
+       description = "Description about Song Number One ..."
+       />
+
+       <CardSongs 
+       title = "Song Number Two"
+       img = "./Picture.jpg"
+       author = "Song Two Author"
+       description = "Description about Song Number Two ..."
+       />
+
+       <CardSongs 
+       title = "Song Number Three"
+       img = "./Picture.jpg"
+       author = "Song Three Author"
+       description = "Description about Song Number Three ..."
+       />
+
+       <CardSongs 
+       title = "Song Number Four"
+       img = "./Picture.jpg"
+       author = "Song Four Author"
+       description = "Description about Song Number Four ..."
+       />
+       
+       <CardSongs 
+       title = "Song Number One"
+       img = "./Picture.jpg"
+       author = "Song One Author"
+       description = "Description about Song Number One ..."
+       />
+
+       <CardSongs 
+       title = "Song Number Two"
+       img = "./Picture.jpg"
+       author = "Song Two Author"
+       description = "Description about Song Number Two ..."
+       />
+
+       <CardSongs 
+       title = "Song Number Three"
+       img = "./Picture.jpg"
+       author = "Song Three Author"
+       description = "Description about Song Number Three ..."
+       />
+
+       <CardSongs 
+       title = "Song Number Four"
+       img = "./Picture.jpg"
+       author = "Song Four Author"
+       description = "Description about Song Number Four ..."
+       />
+       <CardSongs 
+       title = "Song Number One"
+       img = "./Picture.jpg"
+       author = "Song One Author"
+       description = "Description about Song Number One ..."
+       />
+
+       <CardSongs 
+       title = "Song Number Two"
+       img = "./Picture.jpg"
+       author = "Song Two Author"
+       description = "Description about Song Number Two ..."
+       />
+
+       <CardSongs 
+       title = "Song Number Three"
+       img = "./Picture.jpg"
+       author = "Song Three Author"
+       description = "Description about Song Number Three ..."
+       />
+
+       <CardSongs 
+       title = "Song Number Four"
+       img = "./Picture.jpg"
+       author = "Song Four Author"
+       description = "Description about Song Number Four ..."
+       />
       </>
     )
   }
