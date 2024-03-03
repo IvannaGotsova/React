9db4b1c47f742c7a7d399fd3e733d57d420b8044
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-03 20:52:00 +0200

    Change b to h5 tag

--- online-libraries/src/Games.jsx
+++ online-libraries/src/Games.jsx
@@ -5,7 +5,7 @@ function Games() {
     return (
       <>
        <h3>All your Games</h3>
-       <b>Your games are here</b>
+       <h5>Your games are here</h5>
 
        <CardGames 
        title = "Game Number One"
