5a201da4c1c58fa8f0eeb9d417d4ced4504bb32a
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-03 20:52:35 +0200

    Added div tag with className

--- online-libraries/src/CardBooks.jsx
+++ online-libraries/src/CardBooks.jsx
@@ -2,11 +2,13 @@
 function CardBooks(props) {
     return (
         <>
-            <h2 className ="Book-title">{props.title}</h2>
-            <img className ="Book-image" src={props.img} alt="" />
-            <h3 className ="Book-author">{props.author}</h3>
-            <p className ="Book-description">{props.description}</p>
-            <button className ="Book-button">View more...</button>
+            <div className="Book">
+                <h2 className ="Book-title">{props.title}</h2>
+                <img className ="Book-image" src={props.img} alt="" />
+                <h3 className ="Book-author">{props.author}</h3>
+                <p className ="Book-description">{props.description}</p>
+                <button className ="Book-button">View more...</button>
+            </div>
         </>
     )
 }
