f343e8b1c5729484ecfb57db131cbfabceeabd53
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-03 20:52:45 +0200

    Added div tag with className

--- online-libraries/src/CardMovies.jsx
+++ online-libraries/src/CardMovies.jsx
@@ -2,11 +2,13 @@
 function CardMovies(props) {
     return (
         <>
-            <h2 className ="Movie-title">{props.title}</h2>
-            <img className ="Movie-image" src={props.img} alt="" />
-            <h3 className ="Movie-author">{props.author}</h3>
-            <p className ="Movie-description">{props.description}</p>
-            <button className ="Movie-button">View more...</button>
+            <div className="Movie">
+                <h2 className ="Movie-title">{props.title}</h2>
+                <img className ="Movie-image" src={props.img} alt="" />
+                <h3 className ="Movie-author">{props.author}</h3>
+                <p className ="Movie-description">{props.description}</p>
+                <button className ="Movie-button">View more...</button>
+            </div>
         </>
     )
 }
