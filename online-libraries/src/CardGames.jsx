69e29e7f250959e379997c23085135fbaa4cd847
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-03 21:14:12 +0200

    Modified CardGames with className Game-players

--- online-libraries/src/CardGames.jsx
+++ online-libraries/src/CardGames.jsx
@@ -5,7 +5,7 @@ function CardGames(props) {
             <div className="Game">
                 <h2 className ="Game-title">{props.title}</h2>
                 <img className ="Game-image" src={props.img} alt="" />
-                <h3 className ="Game-author">{props.author}</h3>
+                <h3 className ="Game-players">{props.author}</h3>
                 <p className ="Game-description">{props.description}</p>
                 <button className ="Game-button">View more...</button>
             </div>
