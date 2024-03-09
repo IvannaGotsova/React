c97830412cfd7f53af90403051068d6a58390a5e
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-09 20:26:53 +0200

    Created basic Form

---
+++ react-basics-2/src/Form.jsx
@@ -0,0 +1,13 @@
+
+
+function Form() {
+    return (
+      <form>
+        <label>Enter your text:
+          <input type="text" />
+        </label>
+      </form>
+    )
+  }
+
+  export default Form
