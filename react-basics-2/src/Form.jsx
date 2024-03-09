c0770db7ae43a123c1a2aa8d028fbe8859c8b12e
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-09 20:35:04 +0200

    Added other form of Form

--- react-basics-2/src/Form.jsx
+++ react-basics-2/src/Form.jsx
@@ -1,13 +1,20 @@
-
+import { useState } from 'react';
 
 function Form() {
+
+    const [name, setName] = useState("");
+
     return (
-      <form>
-        <label>Enter your text:
-          <input type="text" />
-        </label>
-      </form>
-    )
+        <form>
+          <label>Enter your name:
+            <input
+              type="text" 
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        </form>
+      )
   }
 
   export default Form
