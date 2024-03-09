9240616c850f0351528631fb3573fba171f6b3ba
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-09 21:04:52 +0200

    Created Select

---
+++ react-basics-2/src/Select.jsx
@@ -0,0 +1,20 @@
+
+
+function Select () {
+    return (
+        <select>
+          <option value="One">One</option>
+          <option value="Two">Two</option>
+          <option value="Three">Three</option>
+          <option value="Four">Four</option>
+          <option value="Five">Five</option>
+          <option value="Six">Six</option>
+          <option value="Seven">Seven</option>
+          <option value="Eight">Eight</option>
+          <option value="Nine">Nine</option>
+          <option value="Ten">Ten</option>
+        </select>
+    )
+}
+
+export default Select
