8727a31413681f4d9d6dd4c59927316c71475587
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-09 20:39:13 +0200

    Updated Form with Submit button

--- react-basics-2/src/Form.jsx
+++ react-basics-2/src/Form.jsx
@@ -4,17 +4,23 @@ function Form() {
 
     const [name, setName] = useState("");
 
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      alert(`The name you entered was: ${name}`)
+    }
+
     return (
-        <form>
-          <label>Enter your name:
-            <input
-              type="text" 
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </label>
-        </form>
-      )
+      <form onSubmit={handleSubmit}>
+        <label>Enter your name:
+          <input 
+            type="text" 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+    )
   }
 
   export default Form
