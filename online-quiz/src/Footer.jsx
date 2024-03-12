40c2124b108e183f04dee45104865ddee8eb69b2
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-12 21:21:36 +0200

    Modified Footer with initial data

--- online-quiz/src/Footer.jsx
+++ online-quiz/src/Footer.jsx
@@ -1,7 +1,15 @@
 
 
-function Footer() {
+function Footer () {
 
+    return (
+        <>
+        <hr />
+        <hr />
+        <hr />
+        <h4>Online Quiz - {new Date().getFullYear()}</h4>
+        </>
+    )
 }
 
 export default Footer
