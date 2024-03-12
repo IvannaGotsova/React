1d34bf2930c7079f363d220981adc62dd862a281
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-12 21:18:51 +0200

    Modified Header with initial data

--- online-quiz/src/Header.jsx
+++ online-quiz/src/Header.jsx
@@ -1,7 +1,16 @@
 
 
-function Header() {
+function Header () {
 
+    return (
+        <>
+        <h1>Online Quiz</h1>
+        <h4>Test your knowledge</h4>
+        <hr />
+        <hr />
+        <hr />
+        </>
+    )
 }
 
 export default Header
