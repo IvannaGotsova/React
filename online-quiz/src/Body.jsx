c3740c60bf487df319a84d3d189302b2beb2fb0a
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-12 21:41:09 +0200

    Added const in Body file

--- online-quiz/src/Body.jsx
+++ online-quiz/src/Body.jsx
@@ -2,12 +2,23 @@ import Header from "./Header"
 
 
 function Body() {
-
-    return (
-        <>
-         
-        </>
-    )
-}
+    const [numberOne, setNumberOne] = useState("");
+    const [numberTwo, setNumberTwo] = useState("");
+    const [numberThree, setNumberThree] = useState("");
+    const [numberFour, setNumberFour] = useState("");
+    const [numberFive, setNumberFive] = useState("");
+    const [numberSix, setNumberSix] = useState("");
+    const [numberSeven, setNumberSeven] = useState("");
+    const [numberEight, setNumberEight] = useState("");
+    const [numberNine, setNumberNine] = useState("");
+    const [numberTen, setNumberTen] = useState("");
+    let sentence = ``;
+ 
+   return (
+     <>
+      
+     </>
+   );
+ }
 
 export default Body
