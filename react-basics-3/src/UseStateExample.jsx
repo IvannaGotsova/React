ad5681ee9f949c029175e120852e1a6481a699e8
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-20 22:17:55 +0200

    Renamed file name

---
+++ react-basics-3/src/UseStateExample.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function UseStateExample() {
+
+    const [number, setNumber] = useState(0);
+
+    return (
+        <>
+            <h1>My number is {number}!</h1>
+            <button
+                type="button"
+                onClick={() => setNumber(number + 1)}>Add 1
+            </button>
+            <button
+                type="button"
+                onClick={() => setNumber(number - 1)}>Substract 1
+            </button>
+        </>
+    )
+}
+
+export default UseStateExample
