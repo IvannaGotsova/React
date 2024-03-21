ac7e399b9e74f0b3bc4e7acd7555cafe1d3f43f0
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-21 23:03:32 +0200

    Added OnChangeHandler3 with select

---
+++ react-basics-3/src/OnChangeHandler3.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function OnChangeHandler3 () {
+
+    const [word, setWord] = useState("");
+
+    function handleWordChange(event) {
+        setWord(event.target.value);
+    }
+
+    return (
+        <>
+            <select value={word} onChange={handleWordChange}>
+                <option word={"one"}>one</option>
+                <option word={"two"}>two</option>
+                <option word={"three"}>three</option>
+                <option word={"four"}>four</option>
+                <option word={"five"}>five</option>
+                <option word={"six"}>six</option>
+            </select>
+            <br />
+            <div>The word you choose:- {word}</div>
+        </>
+    )
+  
+}
+export default OnChangeHandler3
