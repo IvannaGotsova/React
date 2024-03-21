ac9587fc0fea99954684cc1b67e7b6a154587491
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-21 22:56:43 +0200

    OnChnageHandler with sleect

---
+++ react-basics-3/src/OnChangeHandler2.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function OnChangeHandler2 () {
+
+    const [number, setNumber] = useState();
+
+    function handleNumberChange(event) {
+        setNumber(event.target.value);
+    }
+
+    return (
+        <>
+            <select value={number} onChange={handleNumberChange}>
+                <option number={1}>1</option>
+                <option number={2}>2</option>
+                <option number={3}>3</option>
+                <option number={4}>4</option>
+                <option number={5}>5</option>
+                <option number={6}>6</option>
+            </select>
+            <br />
+            <div>The number you choose:- {number}</div>
+        </>
+    )
+  
+}
+export default OnChangeHandler2
