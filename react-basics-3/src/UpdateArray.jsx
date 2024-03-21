af8c042ac5725dfe1c728ade2c292bde3b9d9a99
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-21 22:25:42 +0200

    Added functionality to UpdateArray

--- react-basics-3/src/UpdateArray.jsx
+++ react-basics-3/src/UpdateArray.jsx
@@ -1,10 +1,32 @@
+import React, { useState } from 'react';
 
 
 function UpdateArray() {
 
-    return 
-    <>
-    </>
+    const [numbers, setNumbers] = useState([]);
+
+    const addNumber = () => {
+        setNumbers([
+            ...numbers,
+            {
+                id: numbers.length,
+                valueOfNumber: numbers.length + 1
+            }
+        ]);
+    };
+
+    return (
+        <>
+            <ul>
+                {numbers.map(number => (
+                    <li key={number.id}>{number.valueOfNumber}</li>
+                ))}
+            </ul>
+            <button onClick={addNumber}>
+                Add Number
+            </button>
+        </>
+    );
 }
 
 export default UpdateArray
