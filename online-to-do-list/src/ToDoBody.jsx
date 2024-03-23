f0bc9390fa5ca41ea3ac01276a2bd46f1f192d91
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-23 20:34:53 +0200

    Modified to sow created tasks

--- online-to-do-list/src/ToDoBody.jsx
+++ online-to-do-list/src/ToDoBody.jsx
@@ -42,19 +42,24 @@ function ToDoBody() {
 
     return (
         <>
-        <h2>My Tasks</h2>
+         <h2>My Tasks</h2>
         <input type="number" value={taskNumber} placeholder="" onChange={handleTaskNumberChange} /> <br />
         <input type="text" value={taskTitle} placeholder="Your Task Title" onChange={handleTaskTitleChange} /> <br />
         <input type="date" value={taskDate} placeholder="" onChange={handleTaskDateChange} /> <br />
         <input type="text" value={taskDescription} placeholder="Your Task Description" onChange={handleTaskDescriptionChange} /> <br />
         <input type="text" value={taskStatus} placeholder="Your Task Status" onChange={handleTaskStatusChange} /> <br />
         <button onClick={handleAddTask}>Add New Task</button>
+        
         <ol>
-
+            {tasks.map((task, index) => 
+                <li key={index}>
+                    {task.number} {task.title} {task.date} {task.description} {task.status}
+                </li>)}
         </ol>
+        
         <p>The number of your tasks is: {tasks.length}</p>
         </>
-    )
+    );
 }
 
 export default ToDoBody
