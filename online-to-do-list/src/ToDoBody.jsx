b726f79f4ebf5230310273b0087368b72585fc13
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-23 20:00:58 +0200

    Added consts

--- online-to-do-list/src/ToDoBody.jsx
+++ online-to-do-list/src/ToDoBody.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
 
 
 function ToDoBody() {
 
+    const [tasks, setTasks ] = useState([]);
+    const [taskNumber, setTaskNumber ] = useState(0);
+    const [taskTitle, setTaskTitle ] = useState("");
+    const [taskDate, setTaskDate ] = useState(new Date().getDate());
+    const [taskDescription, setTaskDescription ] = useState("");
+    const [taskStatus, setTaskStatus ] = useState("");
+
+
     return (
-        <></>
+        <>
+        </>
     )
 }
 
