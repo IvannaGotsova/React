c0acaa0f40dc5f4025563d46f6743bb6bd54e416
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-23 19:53:55 +0200

    Import ToDoHeader, ToDoBody and TodoFooter tags

--- online-to-do-list/src/App.jsx
+++ online-to-do-list/src/App.jsx
@@ -2,11 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ToDoHeader from './ToDoHeader'
+import ToDoBody from './ToDoBody'
+import ToDoFooter from './ToDoFooter'
 
 function App() {
   
   return (
-    <></>
+    <>
+        <ToDoHeader />
+        <ToDoBody />
+        <ToDoFooter />
+    </>
   )
 }
 
