e4700b0c2fd3e922990b7197686ae480b3854799
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-23 23:25:16 +0200

    Modified - Runs on every render

--- react-basics-3/src/UseEffectExample.jsx
+++ react-basics-3/src/UseEffectExample.jsx
@@ -3,8 +3,17 @@ import { useState, useEffect } from "react";
 
 function UseEffectExample () {
 
+    const [number, setNumber] = useState(0);
+
+    useEffect(() => {
+        document.title = `Times you clicked ${number}`;
+    });
+
     return (
         <>
+            <button onClick={() => setNumber((n) => n + 1)}>
+                Click </button>
+            <p>You clicked {number}</p>
         </>
     )
 }
