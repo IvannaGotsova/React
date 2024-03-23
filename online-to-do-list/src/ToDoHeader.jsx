3a6ada8359f7e06703bd9b58b45b0cbe76a7497d
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-23 19:53:25 +0200

    Added functionality to Header

--- online-to-do-list/src/ToDoHeader.jsx
+++ online-to-do-list/src/ToDoHeader.jsx
@@ -3,7 +3,13 @@
 function ToDoHeader() {
 
     return (
-        <></>
+        <>
+        <h1>Online TODO List</h1>
+        <h2>Organize your tasks</h2>
+        <hr />
+        <hr />
+        <hr />
+        </>
     )
 }
 
