1d1b5ee87a66282633eeb7821227bf6bae30ebfa
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-24 14:13:50 +0200

    Import UseRefExample tag

--- react-basics-4/src/App.jsx
+++ react-basics-4/src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import First from './First'
+import UseEffectExample3 from './UseEffectExample3'
 
 function App() {
   
   return (
     <>
+        <UseEffectExample3/>
         <First />
     </>
   )
