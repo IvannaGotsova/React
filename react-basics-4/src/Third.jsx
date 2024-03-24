3319b83cf39319e2b3b5a13479c60d48a9a992c3
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-24 14:10:36 +0200

    Import useContext()

--- react-basics-4/src/Third.jsx
+++ react-basics-4/src/Third.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react"
+import { useContext } from "react";
+import { WordContext } from "./First";
 import Fourth from "./Fourth"
 
 function Third() {
   
+    const word = useContext(WordContext);
+
     return (
-        <div className="example">
+        <div className="example">         
           <p>This is Third</p>
+          <p>{`${word}`}</p>
           <Fourth />
         </div>
       );
