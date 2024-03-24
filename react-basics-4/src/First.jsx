bcd63dda31deab5ea923ad6a27f53a5377d38e1f
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-24 10:46:29 +0200

    Import Component and basic functionality

--- react-basics-4/src/First.jsx
+++ react-basics-4/src/First.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react"
+import Second from "./Second"
 
 
 function First() {
   
     return (
-      <>
-        
-      </>
-    )
+      <div className="example">
+        <p>This is First</p>
+        <Second />
+      </div>
+    );
   }
   
-  export default First
+export default First
