8d3c8c213af99fc5562eca03f0b91dbed25faf68
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-24 10:46:46 +0200

    Import Component and basic functionality

--- react-basics-4/src/Fourth.jsx
+++ react-basics-4/src/Fourth.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react"
+import Fifth from "./Fifth"
 
 
 function Fourth() {
   
     return (
-      <>
-        
-      </>
-    )
+        <div className="example">
+          <p>This is Fourth</p>
+          <Fifth />
+        </div>
+      );
   }
   
-  export default Fourth
+export default Fourth
