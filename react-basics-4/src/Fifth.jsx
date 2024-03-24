a0a1bf37a21b0bcb042b7c2bd7af82152873870d
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-24 14:10:50 +0200

    Import useContext()

--- react-basics-4/src/Fifth.jsx
+++ react-basics-4/src/Fifth.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react"
-import Fourth from "./Fourth"
+import { useContext } from "react";
+import { WordContext } from "./First";
 
 
 function Fifth() {
   
+    const word = useContext(WordContext);
+
     return (
         <div className="example">
           <p>This is Fifth</p>
+          <p>{`${word}`}</p>
         </div>
       );
   }
