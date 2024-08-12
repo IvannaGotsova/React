9d2fad38c7fb50e75191681dfbefbf917025b5c9
IvannaGotsova <ivannagotsova@outlook.com>
2024-08-12 21:49:20 +0300

    Use Effect Example

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UseStateHook from './UseStateHook'
+import UseEffectHook from './UseStateEffect'
 
 function App() {
 
   return (
     <>
       <UseStateHook />
+      <UseEffectHook />
     </>
   )
 }
