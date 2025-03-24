ebb0763c1651ca0a6f9041184a62c8f34ed875c0
IvannaGotsova <ivannagotsova@outlook.com>
2025-03-24 10:56:05 +0200

    Added content for users

---
+++ Authentication/src/Component.AllUsers.jsx
@@ -0,0 +1,9 @@
+function ComponentAllUsers () {
+    return (
+        <>
+          <p>Visible to All Users</p>
+        </>
+    )
+}
+
+export default ComponentAllUsers
---
+++ Authentication/src/ComponentLoginUsers.jsx
@@ -0,0 +1,9 @@
+function ComponentLoginUsers () {
+    return (
+        <>
+          <p>Visible to Login Users</p>
+        </>
+    )
+}
+
+export default ComponentLoginUsers
