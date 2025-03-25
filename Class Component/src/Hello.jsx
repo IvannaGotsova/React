8819825886fead74563d28e6894ba4ef6f696c8c
IvannaGotsova <ivannagotsova@outlook.com>
2025-03-25 11:20:22 +0200

    Class Component

---
+++ Class Component/src/Hello.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Hello extends React.Component {
+
+    render () {
+        return (
+        <>
+          <h1>Hello</h1>
+          <h2>Hello</h2>
+          <h3>Hello</h3>
+          <h4>Hello</h4>
+          <h5>Hello</h5>
+          <h6>Hello</h6>
+        </>
+        )
+  }
+}
+
+export default Hello
