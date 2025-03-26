ab99002d14e7dddeaa59250bb1172ef1f179ab29
IvannaGotsova <ivannagotsova@outlook.com>
2025-03-26 08:10:06 +0200

    Creating Events

---
+++ Events/src/CreatingEvents.jsx
@@ -0,0 +1,11 @@
+function CreatingEvents () {
+    const greeting = () => {
+        alert("Hello");
+      }
+    
+      return (
+        <><button type="button" onClick={greeting}>Click</button></>
+      )
+}
+
+export default CreatingEvents
