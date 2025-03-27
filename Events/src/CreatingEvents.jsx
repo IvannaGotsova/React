3cf4553bc2b725e0f91d3fc4de7c8b11ef50d92b
IvannaGotsova <ivannagotsova@outlook.com>
2025-03-27 10:44:47 +0200

    Add Argument

--- Events/src/CreatingEvents.jsx
+++ Events/src/CreatingEvents.jsx
@@ -1,10 +1,13 @@
 function CreatingEvents () {
-    const greeting = () => {
-        alert("Hello");
+    const greeting = (props) => {
+        let text = "Hello";
+        let add = props
+        let result = text.concat(" ", add);
+        alert(result);
       }
     
       return (
-        <><button type="button" onClick={greeting}>Click</button></>
+        <><button type="button" onClick={() => greeting("User")}>Click</button></>
       )
 }
 
